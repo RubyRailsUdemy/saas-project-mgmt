0ad0bd7f80dd4570c2c6fbf0016fc12b4e55ed00
David Fountain <savsin64@gmail.com>
2018-03-17 15:35:51 -0700

    Create Projects

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,9 @@
 //= require jquery
 //= require twitter/bootstrap
 //= require_tree .
+
+$(document).ready(function($) {
+    $(".clickable-row").click(function() {
+        window.location = $(this).data("href");
+    });
+});
